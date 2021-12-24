dc9eab12e6d093844e21572c72d2ac70b396ba13
Blueything <48907750+Blueything@users.noreply.github.com>
2021-12-24 20:17:46 +0530

    Update fully_optimized_prime.java

--- Prime/fully_optimized_prime.java
+++ Prime/fully_optimized_prime.java
@@ -1,34 +1,42 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
 
 import java.util.*;
 
-class Main{
-  public static void main(String args[])
+class Main
+{
+  public static void main (String args[])
   {
     int num;
+    Scanner sc = new Scanner (System.in);
+    num = sc.nextInt ();
+    int last = (int) Math.sqrt (num);
     boolean flag=false;
-    Scanner sc = new Scanner(System.in);
-    num = sc.nextInt();
-    
-    if(num%2==0)
-    {
-        System.out.println("Not prime");
-    }
+    if (num % 2 == 0)
+      {
+    	System.out.println ("Not prime");
+      }
     else
-    {
-        for(int i =3; i<num;i=i+2)
-        {
-          if(num%i==0)
-          {
-            System.out.println("Not Prime");
-            flag=true;
-            break;
-          }
-          
-        }
-    }
-    if(flag==false)
-    {
-      System.out.println("Prime");
-    }
+      {
+    	for (int i = 3; i < (last); i = i + 2)
+    	  {
+    	    if (num % i == 0)
+    	      {
+        		System.out.println ("Not Prime");
+        		flag =true;
+        		break;
+    	      }
+    	   
+    	  }
+      }
+      if(flag==false)
+      {
+          System.out.println("Prime");
+      }
   }
 }
