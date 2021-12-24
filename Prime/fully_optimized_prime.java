8f7033fa0ae10849d293f0a6615e889e578bb730
Blueything <48907750+Blueything@users.noreply.github.com>
2021-12-24 20:16:03 +0530

    Create fully_optimized_prime.java

---
+++ Prime/fully_optimized_prime.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+class Main{
+  public static void main(String args[])
+  {
+    int num;
+    boolean flag=false;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    
+    if(num%2==0)
+    {
+        System.out.println("Not prime");
+    }
+    else
+    {
+        for(int i =3; i<num;i=i+2)
+        {
+          if(num%i==0)
+          {
+            System.out.println("Not Prime");
+            flag=true;
+            break;
+          }
+          
+        }
+    }
+    if(flag==false)
+    {
+      System.out.println("Prime");
+    }
+  }
+}
